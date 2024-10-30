39650b40d94932128eea7c0de678b0e4828f0562
evidal1990 <ecsr.vidal@gmail.com>
2024-10-30 08:46:58 -0300

    Criando endpoint para salvar um gênero

--- genres/views.py
+++ genres/views.py
@@ -1,9 +1,17 @@
 from django.http import JsonResponse
 from genres.models import Genre
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
+@csrf_exempt
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{"id": genre.id, "name": genre.name}
-            for genre in genres]
-    return JsonResponse(data, safe=False)
+    if request.method == "GET":
+        genres = Genre.objects.all()
+        data = [{"id": genre.id, "name": genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == "POST":
+        data = json.loads(request.body.decode("utf-8"))
+        new_genre = Genre(name=data["name"])
+        new_genre.save()
+        return JsonResponse({"id": new_genre.id, "name": new_genre.name}, status=201)
