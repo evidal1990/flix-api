5dd842a670c0c1c2339210d2a072710763662458
evidal1990 <ecsr.vidal@gmail.com>
2024-11-07 08:45:13 -0300

    Adicionando validações aos campos de data de lançamento, resumo e retornando a média de avaliações dos filmes

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,29 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 
 class MovieSerializers(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError(
+                "A data de lancamento deve ser posterior a 1900")
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 255:
+            raise serializers.ValidationError(
+                "O resumo deve ter no maximo 255 caracteres")
+        return value
+
+    def get_rate(self, object):
+        rate = object.reviews.aggregate(Avg("stars")).get("stars__avg")
+        if (rate):
+            return round(rate)
+        return None
