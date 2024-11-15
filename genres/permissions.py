fbf71d6a164416655da717a510269f0e25937fbb
evidal1990 <ecsr.vidal@gmail.com>
2024-11-15 19:29:57 -0300

    Finalizando as verificações de permissão em Genre

--- genres/permissions.py
+++ genres/permissions.py
@@ -7,8 +7,8 @@ class GenrePermission(permissions.BasePermission):
             return request.user.has_perm("genres.view_genre")
         if request.method == "POST":
             return request.user.has_perm("genres.add_genre")
-        if request.method == "PUT":
-				return request.user.has_perm("genres.change_genre")
-		  if request.method == "DELETE":
-				return request.user.has_perm("genres.delete_genre")
+        if request.method in ["PUT", "PATCH"]:
+            return request.user.has_perm("genres.change_genre")
+        if request.method == "DELETE":
+            return request.user.has_perm("genres.delete_genre")
         return False
